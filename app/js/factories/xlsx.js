2aba9fedde42bd7527829f0a906adb288b9c3fe5
Sascha <sascha.grothe@gmail.com>
2014-04-29 22:58:55 +0200

    Generate xlsx file and add value

--- app/js/factories/xlsx.js
+++ app/js/factories/xlsx.js
@@ -1,13 +1,53 @@
 (function() {
-  var module;
+  var module, xml2js;
+
+  xml2js = require('xml2js');
 
   module = angular.module('XlsxFactory', []);
 
   module.factory('Xlsx', function() {
     return {
-      zip: new require('jszip'),
+      zip: require('jszip'),
+      fs: require('fs'),
+      parser: new xml2js.Parser(),
+      builder: new xml2js.Builder(),
+      xlsx: {},
+      loadTemplate: function() {
+        var file;
+        file = window.location.pathname.split("views")[0] + 'templates/template.xlsx';
+        return this.xlsx = new this.zip(this.fs.readFileSync(file));
+      },
       log: function() {
-        return console.log(this.zip);
+        var _this = this;
+        this.loadTemplate();
+        console.log(this.xlsx.file('xl/worksheets/sheet1.xml').asText());
+        return this.parser.parseString(this.xlsx.file('xl/worksheets/sheet2.xml').asText(), function(err, result) {
+          var buffer, data;
+          console.log(result);
+          result.worksheet.sheetData = {
+            row: {
+              '$': {
+                r: 1
+              },
+              c: {
+                '$': {
+                  r: 'A1',
+                  t: 'n'
+                },
+                v: 345
+              }
+            }
+          };
+          _this.builder.options.renderOpts.pretty = false;
+          data = _this.builder.buildObject(result);
+          _this.xlsx.file('xl/worksheets/sheet1.xml', data);
+          buffer = _this.xlsx.generate({
+            type: 'nodebuffer'
+          });
+          return _this.fs.writeFile(window.location.pathname.split("views")[0] + 'templates/test.xlsx', buffer, function(err) {
+            return console.log(err);
+          });
+        });
       }
     };
   });
--- src/coffee/factories/xlsx.coffee
+++ src/coffee/factories/xlsx.coffee
@@ -1,8 +1,36 @@
+xml2js = require 'xml2js'
 module = angular.module 'XlsxFactory', []
 
 module.factory 'Xlsx', () ->
   {
-    zip: new require('jszip')
+    zip: require 'jszip'
+    fs: require 'fs'
+    parser: new xml2js.Parser()
+    builder: new xml2js.Builder()
+    xlsx: {}
+    loadTemplate: () ->
+      file = window.location.pathname.split("views")[0] + 'templates/template.xlsx'
+      @xlsx = new @zip(@fs.readFileSync(file))
     log: () ->
-      console.log @zip
+      @loadTemplate()
+      console.log @xlsx.file('xl/worksheets/sheet1.xml').asText()
+      @parser.parseString @xlsx.file('xl/worksheets/sheet2.xml').asText(), (err, result) =>
+        console.log result
+        result.worksheet.sheetData = {
+          row:
+            '$':
+              r: 1
+            c:
+              '$':
+                r: 'A1'
+                t: 'n'
+              v: 345
+        }
+        @builder.options.renderOpts.pretty = false
+        data = @builder.buildObject(result)
+        @xlsx.file('xl/worksheets/sheet1.xml', data)
+        buffer = @xlsx.generate {type: 'nodebuffer'}
+        @fs.writeFile window.location.pathname.split("views")[0] + 'templates/test.xlsx', buffer, (err) =>
+          console.log err
+      # console.log @fs.existsSync(file)
   }
