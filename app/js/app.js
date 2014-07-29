1f1a12a37f8cfe705db62633a60375fe29cf20f1
Sascha <sascha.grothe@gmail.com>
2014-07-29 21:18:30 +0200

    Added routes

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,31 @@
 (function() {
-  this.App = angular.module('dendrites', ['Controllers', 'dendritesDirectives', 'readXlsFactory', 'XlsxFactory', 'dataFactory', 'dataStoreFactory']);
+  this.App = angular.module('dendrites', ['ui.router', 'Controllers', 'dendritesDirectives', 'readXlsFactory', 'XlsxFactory', 'dataFactory', 'dataStoreFactory']);
+
+  this.App.config([
+    '$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider.otherwise('/source');
+      return $stateProvider.state('source', {
+        url: '/source',
+        templateUrl: '../views/source.html',
+        controller: 'sourceCtrl'
+      }).state('groups', {
+        url: '/groups',
+        templateUrl: '../views/groups.html',
+        controller: 'groupsCtrl'
+      }).state('data', {
+        url: '/data',
+        templateUrl: '../views/data.html',
+        controller: 'dataCtrl'
+      }).state('preview', {
+        url: '/preview',
+        templateUrl: '../views/preview.html',
+        controller: 'previewCtrl'
+      }).state('export', {
+        url: '/export',
+        templateUrl: '../views/export.html',
+        controller: 'exportCtrl'
+      });
+    }
+  ]);
 
 }).call(this);
--- src/coffee/app.coffee
+++ src/coffee/app.coffee
@@ -1,10 +1,44 @@
-# initialize angular append
+# initialize angular app
 
 @App = angular.module 'dendrites', [
+  'ui.router',
   'Controllers',
   'dendritesDirectives',
   'readXlsFactory',
   'XlsxFactory',
   'dataFactory',
   'dataStoreFactory'
+]
+
+# config routes
+
+@App.config ['$stateProvider', '$urlRouterProvider', ($stateProvider, $urlRouterProvider) ->
+  $urlRouterProvider.otherwise '/source'
+
+  $stateProvider
+    .state('source', {
+      url: '/source'
+      templateUrl: '../views/source.html'
+      controller: 'sourceCtrl'
+    })
+    .state('groups', {
+      url: '/groups'
+      templateUrl: '../views/groups.html'
+      controller: 'groupsCtrl'
+    })
+    .state('data', {
+      url: '/data'
+      templateUrl: '../views/data.html'
+      controller: 'dataCtrl'
+    })
+    .state('preview', {
+      url: '/preview'
+      templateUrl: '../views/preview.html'
+      controller: 'previewCtrl'
+    })
+    .state('export', {
+      url: '/export'
+      templateUrl: '../views/export.html'
+      controller: 'exportCtrl'
+    })
 ]
